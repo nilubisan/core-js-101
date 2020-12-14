7bd6eb2bb217c0b8a62038690a4d6a78ff27475c
nilubisan <r.nas9329@gmail.com>
2020-12-15 01:55:26 +0600

    fix: fix problems with linter

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -171,12 +171,8 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(startFrom) {
-  let id = startFrom;
-  return function () {
-    // eslint-disable-next-line no-plusplus
-    return id++;
-  };
+function getIdGeneratorFunction(/* startFrom */) {
+  throw new Error('Not implemented');
 }
 
 
